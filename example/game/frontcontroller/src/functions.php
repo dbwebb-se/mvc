fa52edace14ab10eb01a5d2e43a0f216ab68bbdd
Mikael Roos <mikael.t.h.roos@gmail.com>
2021-03-29 20:38:15 +0200

    Remove linter errors

--- example/game/frontcontroller/src/functions.php
+++ example/game/frontcontroller/src/functions.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Mos\Functions;
 
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
 /**
  * Functions.
  */
@@ -64,13 +67,13 @@ function renderTwigView(
     static $twig = null;
 
     if (is_null($twig)) {
-        $loader = new \Twig\Loader\FilesystemLoader(
+        $loader = new FilesystemLoader(
             INSTALL_PATH . "/view/twig"
         );
         // $twig = new \Twig\Environment($loader, [
         //     "cache" => INSTALL_PATH . "/cache/twig",
         // ]);
-        $twig = new \Twig\Environment($loader);
+        $twig = new Environment($loader);
     }
 
     return $twig->render($template, $data);
