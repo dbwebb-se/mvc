1c1e9d2990aa01250d1ea786b9a4eb102bc270ef
Mikael Roos <mikael.t.h.roos@gmail.com>
2021-04-08 11:26:38 +0200

    Use the factory to create the response object, #6

--- example/game/router/src/Controller/Debug.php
+++ example/game/router/src/Controller/Debug.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Mos\Controller;
 
-use Nyholm\Psr7\Response;
-use Nyholm\Psr7\Stream;
+// use Nyholm\Psr7\Response;
+// use Nyholm\Psr7\Stream;
+use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Http\Message\ResponseInterface;
 
 use function Mos\Functions\renderView;
@@ -19,8 +20,19 @@ class Debug
     {
         $body = renderView("layout/debug.php");
 
-        return (new Response())
-            ->withStatus(200)
-            ->withBody(Stream::create($body));
+        // Create and return the response
+        $psr17Factory = new Psr17Factory();
+        return $psr17Factory
+            ->createResponse(200)
+            ->withBody($psr17Factory->createStream($body));
     }
+
+    // public function __invoke(): ResponseInterface
+    // {
+    //     $body = renderView("layout/debug.php");
+    //
+    //     return (new Response())
+    //         ->withStatus(200)
+    //         ->withBody(Stream::create($body));
+    // }
 }
