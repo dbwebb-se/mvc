7899f62cb5c56d37b869996e9811a0f07171dbc9
Mikael Roos <mikael.t.h.roos@gmail.com>
2021-04-13 11:45:42 +0200

    Updates to support the phpunit example in sample game code

--- example/game/phpunit/config/bootstrap.php
+++ example/game/phpunit/config/bootstrap.php
@@ -11,7 +11,7 @@ error_reporting(-1);                // Report all type of errors
 ini_set("display_errors", "1");     // Display all errors
 
 // Start the session
-if(php_sapi_name() !== "cli") {
+if (php_sapi_name() !== "cli") {
     session_name(preg_replace("/[^a-z\d]/i", "", __DIR__));
     session_start();
 }
