6e4fa3bce02aae79614eaba93be3f45b627d0443
Mikael Roos <mikael.t.h.roos@gmail.com>
2023-04-04 17:23:01 +0200

    Fix app code after walkthrough

--- example/symfony-write-application-code/app/src/Controller/DiceGameController.php
+++ example/symfony-write-application-code/app/src/Controller/DiceGameController.php
@@ -5,12 +5,12 @@ namespace App\Controller;
 use App\Dice\Dice;
 use App\Dice\DiceGraphic;
 use App\Dice\DiceHand;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 class DiceGameController extends AbstractController
 {
@@ -42,7 +42,6 @@ class DiceGameController extends AbstractController
 
         $diceRoll = [];
         for ($i = 1; $i <= $num; $i++) {
-            //$die = new Dice();
             $die = new DiceGraphic();
             $die->roll();
             $diceRoll[] = $die->getAsString();
@@ -55,6 +54,7 @@ class DiceGameController extends AbstractController
 
         return $this->render('pig/test/roll_many.html.twig', $data);
     }
+
 
     #[Route("/game/pig/test/dicehand/{num<\d+>}", name: "test_dicehand")]
     public function testDiceHand(int $num): Response
@@ -88,6 +88,7 @@ class DiceGameController extends AbstractController
         return $this->render('pig/init.html.twig');
     }
 
+
     #[Route("/game/pig/init", name: "pig_init_post", methods: ['POST'])]
     public function initCallback(
         Request $request,
@@ -96,17 +97,16 @@ class DiceGameController extends AbstractController
     {
         $numDice = $request->request->get('num_dices');
 
+        $hand = new DiceHand();
+        for ($i = 1; $i <= $numDice; $i++) {
+            $hand->add(new DiceGraphic());
+        }
+        $hand->roll();
+
+        $session->set("pig_dicehand", $hand);
         $session->set("pig_dices", $numDice);
         $session->set("pig_round", 0);
         $session->set("pig_total", 0);
-
-        $dice = [];
-        for ($i = 1; $i <= $numDice; $i++) {
-            $die = new DiceGraphic();
-            $die->roll();
-            $dice[] = $die;
-        }
-        $session->set("pig_dice", $dice);
 
         return $this->redirectToRoute('pig_play');
     }
@@ -116,20 +116,13 @@ class DiceGameController extends AbstractController
         SessionInterface $session
     ): Response
     {
-        $dice = $session->get("pig_dice");
-        $diceValues = [];
-        foreach ($dice as $die) {
-            $diceValues[] = $die->getAsString();
-        }
+        $dicehand = $session->get("pig_dicehand");
 
         $data = [
-            "rollUrl" => $this->generateUrl('pig_roll'),
-            "saveUrl" => $this->generateUrl('pig_save'),
-            "restartUrl" => $this->generateUrl('pig_init_get'),
             "pigDices" => $session->get("pig_dices"),
             "pigRound" => $session->get("pig_round"),
             "pigTotal" => $session->get("pig_total"),
-            "diceValues" => $diceValues 
+            "diceValues" => $dicehand->getString() 
         ];
 
         return $this->render('pig/play.html.twig', $data);
@@ -140,15 +133,13 @@ class DiceGameController extends AbstractController
         SessionInterface $session
     ): Response
     {
-        $dice = $session->get("pig_dice");
-        foreach ($dice as $die) {
-           $die->roll();
-        }
+        $hand = $session->get("pig_dicehand");
+        $hand->roll();
 
         $roundTotal = $session->get("pig_round");
         $round = 0;
-        foreach ($dice as $die) {
-            $value = $die->getValue();
+        $values = $hand->getValues();
+        foreach ($values as $value) {
             if ($value === 1) {
                 $round = 0;
                 $roundTotal = 0;
@@ -184,4 +175,5 @@ class DiceGameController extends AbstractController
 
         return $this->redirectToRoute('pig_play');
     }
+
 }
