a79d812e382cc67a62f0c067e62d7e319e368709
Mikael Roos <mikael.t.h.roos@gmail.com>
2023-04-28 09:25:59 +0200

    fix gui

--- .dbwebb/inspect-src/kmom.d/pre_download.bash
+++ .dbwebb/inspect-src/kmom.d/pre_download.bash
@@ -17,40 +17,48 @@
 #printf ">>> -------------- Pre download (all kmoms) ----------------------\n"
 
 # Open the student home page
-studentServerUrl="$REDOVISA_HTTP_PREFIX/~$ACRONYM/dbwebb-kurser/$COURSE/me/report/public"
-openUrl "$studentServerUrl/public/report#$KMOM"
+studentServerUrl="$REDOVISA_HTTP_PREFIX/~$ACRONYM/dbwebb-kurser/$COURSE/me"
+openUrl "$studentServerUrl/report/public/report#$KMOM"
 
 # Will not work on cygwin
 #sudo rm -rf me/report/build
 
 case $KMOM in
     kmom01)
-        openUrl "$studentServerUrl/public/"
-        openUrl "$studentServerUrl/public/about"
-        openUrl "$studentServerUrl/public/lucky"
-        openUrl "$studentServerUrl/public/api/quote"
+        openUrl "$studentServerUrl/report/public/"
+        openUrl "$studentServerUrl/report/public/about"
+        openUrl "$studentServerUrl/report/public/lucky"
+        openUrl "$studentServerUrl/report/public/api/quote"
     ;;
     kmom02)
-        openUrl "$studentServerUrl/public/"
-        openUrl "$studentServerUrl/public/api"
-        openUrl "$studentServerUrl/public/card"
+        openUrl "$studentServerUrl/report/public/"
+        openUrl "$studentServerUrl/report/public/api"
+        openUrl "$studentServerUrl/report/public/card"
     ;;
     kmom03)
-        openUrl "$studentServerUrl/public/"
-        openUrl "$studentServerUrl/public/game/"
-        openUrl "$studentServerUrl/public/game/doc/"
-        openUrl "$studentServerUrl/public/api"
+        openUrl "$studentServerUrl/report/public/"
+        openUrl "$studentServerUrl/report/public/game/"
+        openUrl "$studentServerUrl/report/public/game/doc/"
+        openUrl "$studentServerUrl/report/public/api"
     ;;
     kmom04)
-        openUrl "$studentServerUrl/public/"
-        openUrl "$studentServerUrl/docs/api/"
-        openUrl "$studentServerUrl/docs/coverage/"
+        openUrl "$studentServerUrl/report/public/"
+        openUrl "$studentServerUrl/report/docs/api/"
+        openUrl "$studentServerUrl/report/docs/coverage/"
     ;;
     kmom05)
+        openUrl "$studentServerUrl/report/public/"
+        openUrl "$studentServerUrl/report/docs/api/"
     ;;
     kmom06)
+        openUrl "$studentServerUrl/report/public/"
+        openUrl "$studentServerUrl/proj/docs/metrics/"
     ;;
     kmom10)
-        openUrl "$studentServerUrl/proj"
+        openUrl "$studentServerUrl/report/public/"
+        openUrl "$studentServerUrl/proj/public"
+        openUrl "$studentServerUrl/proj/docs/api/"
+        openUrl "$studentServerUrl/proj/docs/coverage/"
+        openUrl "$studentServerUrl/proj/docs/metrics/"
         ;;
 esac
