1f8e02e3fbe559e48e4ce2bc766f306a88492857
Mikael Roos <mikael.t.h.roos@gmail.com>
2023-08-31 15:55:28 +0200

    fix git

--- .dbwebb/inspect-src/kmom.d/report/807_me-report_check-repo-remote.bash
+++ .dbwebb/inspect-src/kmom.d/report/807_me-report_check-repo-remote.bash
@@ -6,6 +6,9 @@ TARGET_DIR="me/report"
 cd $TARGET_DIR || exit 1
 [[ ! -d .git ]] && echo "Missing .git directory." && exit 1
 
+# git v 2.30.2 otherwise error message
+git config --global --add safe.directory /home/dbwebb/repo/me/report 
+
 gitUrl=$( git config --get remote.origin.url )
 [[ $gitUrl ]]
 doLog $? "remote = '$gitUrl'" 1
